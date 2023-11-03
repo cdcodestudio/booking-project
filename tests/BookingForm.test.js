c51c272a8ca1f4e05807e8b869e7a617882b901e
cardcodes <cdcodestuff@gmail.com>
2023-11-03 03:26:26 -0500

    Add tests

--- tests/BookingForm.test.js
+++ tests/BookingForm.test.js
@@ -1,41 +1,30 @@
-/* eslint-disable no-undef */
-/* eslint-disable no-unused-vars */
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { render, fireEvent, act } from '@testing-library/react';
 import BookingForm from '../src/components/BookingForm';
+import { MemoryRouter } from 'react-router-dom';
+import '@testing-library/jest-dom/';
 
-test('renders the form with initial state', () => {
-  render(<BookingForm />);
-
-  // Check that the Name field is rendered
-  expect(screen.getByLabelText(/name/i)).toBeInTheDocument();
-  // Check that the Phone field is rendered
-  expect(screen.getByLabelText(/phone/i)).toBeInTheDocument();
-  // Check that the Email field is rendered
-  expect(screen.getByLabelText(/email/i)).toBeInTheDocument();
+test('it renders without crashing', () => {
+  render(
+    <MemoryRouter>
+      <BookingForm />
+    </MemoryRouter>
+  );
 });
 
-test('shows validation errors for the first three fields', async () => {
-  render(<BookingForm />);
+test('it does not submit the form when the name input is empty', async () => {
+  const { getByLabelText, getByText } = render(
+    <MemoryRouter>
+      <BookingForm />
+    </MemoryRouter>
+  );
+  const nameInput = getByLabelText('Name');
+  const submitButton = getByText('Confirm Reservation');
 
-  // Click the submit button to trigger validation
-  userEvent.click(screen.getByText(/confirm reservation/i));
+  await act(async () => {
+    fireEvent.click(submitButton);
+  });
 
-  // Check for validation error messages
-  expect(await screen.findByText('This is required')).toBeInTheDocument();
-  expect(await screen.findByText('Invalid phone number format')).toBeInTheDocument();
-  expect(await screen.findByText('This is required')).toBeInTheDocument();
-
-  // Fill in the fields with valid data
-  userEvent.type(screen.getByLabelText(/name/i), 'John Doe');
-  userEvent.type(screen.getByLabelText(/phone/i), '123-456-7890');
-  userEvent.type(screen.getByLabelText(/email/i), 'john.doe@example.com');
-
-  // Click the submit button again
-  userEvent.click(screen.getByText(/confirm reservation/i));
-
-  // Check that the error messages are gone
-  expect(screen.queryByText('This is required')).not.toBeInTheDocument();
-  expect(screen.queryByText('Invalid phone number format')).not.toBeInTheDocument();
+  expect(nameInput).toBeInvalid();
+  expect(nameInput).toHaveAttribute('aria-invalid', 'true');
 });
